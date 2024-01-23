5bef30824529f47c07cd370ba08d8f769aa77dd2
johncop <thoanglong94@gmail.com>
2024-01-23 19:10:51 +0700

    Add packageController

---
+++ PartyPalKiddosAPI/Controllers/PackageController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Interface;
+using Repository.Repository;
+
+namespace PartyPalKiddosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        private IPackageRepository repository = new PackageRepository();
+
+        [HttpGet("GetPackage")]
+        public ActionResult<IEnumerable<Package>> getPackage()
+            => repository.GetAllPackage();
+
+        [HttpGet("Package/{packageId}")]
+        public ActionResult<Package> getPackageById(int packageId) =>
+            repository.GetPackageById(packageId);
+    }
+}
