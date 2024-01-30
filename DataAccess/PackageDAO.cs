15d977ab236800b8c551d8293227ee248ce9bc27
johncop <thoanglong94@gmail.com>
2024-01-30 08:23:07 +0700

    update getPackage function

--- DataAccess/PackageDAO.cs
+++ DataAccess/PackageDAO.cs
@@ -19,66 +19,13 @@ namespace DataAccess
                 using (var context = new PartyPalKiddosContext())
                 {
                     listPackage = context.Packages
-                .Include(p => p.OrderFood)
-                .ThenInclude(of => of.Food)
-                .Include(p => p.OrderDrink)
-                .ThenInclude(of => of.Drink)
-                .Include(p => p.OrderService)
-                .ThenInclude(of => of.Service)
-                .Include(p => p.User)
                 .Select(package => new Package
                 {
                     Id = package.Id,
                     PackageName = package.PackageName,
-                    OrderDrinkId = package.OrderDrinkId,
-                    OrderFoodId = package.OrderFoodId,
-                    OrderServiceId = package.OrderServiceId,
                     UserId = package.UserId,
                     PackageCategoryId = package.PackageCategoryId,
-                    Status = package.Status,
-                    // Assuming you have navigation properties in your Package entity
-                    OrderFood = package.OrderFood != null ? new OrderFood
-                    {
-                        Id = package.OrderFood.Id,
-                        FoodId = package.OrderFood.FoodId,
-                        Quantity = package.OrderFood.Quantity,
-                        Food = package.OrderFood.Food != null ? new Food
-                        {
-                            Id = package.OrderFood.Id,
-                            FoodName = package.OrderFood.Food.FoodName,
-                            Description = package.OrderFood.Food.Description,
-                            Price = package.OrderFood.Food.Price,
-                        } : null,
-                    } : null,
-                    OrderDrink = package.OrderDrink != null ? new OrderDrink
-                    {
-                        Id = package.OrderDrink.Id,
-                        DrinkId = package.OrderDrink.DrinkId,
-                        Quantity = package.OrderDrink.Quantity,
-                        Drink = package.OrderDrink.Drink != null ? new Drink
-                        {
-                            Id = package.OrderDrink.Id,
-                            DrinkName = package.OrderDrink.Drink.DrinkName,
-                            Description = package.OrderDrink.Drink.Description,
-                            Price = package.OrderDrink.Drink.Price,
-
-                        } : null,
-                    } : null,
-                    OrderService = package.OrderService != null ? new OrderService
-                    {
-                        Id = package.OrderService.Id,
-                        ServiceId = package.OrderService.ServiceId,
-                        Quantity = package.OrderService.Quantity,
-                        Service = package.OrderService.Service != null ? new Service
-                        {
-                            Id = package.OrderDrink.Id,
-                            ServiceName = package.OrderService.Service.ServiceName,
-                            Description = package.OrderService.Service.Description,
-                            Price = package.OrderService.Service.Price,
-
-                        } : null,
-                    } : null
-                    // If there are more fields, map them accordingly
+                    Status = package.Status               
                 }).ToList();
                 }
             }
@@ -106,32 +53,9 @@ namespace DataAccess
                 {
                     Id = package.Id,
                     PackageName = package.PackageName,
-                    OrderDrinkId = package.OrderDrinkId,
-                    OrderFoodId = package.OrderFoodId,
-                    OrderServiceId = package.OrderServiceId,
                     UserId = package.UserId,
                     PackageCategoryId = package.PackageCategoryId,
-                    Status = package.Status,
-                    // Assuming you have navigation properties in your Package entity
-                    OrderFood = package.OrderFood != null ? new OrderFood
-                    {
-                        Id = package.OrderFood.Id,
-                        FoodId = package.OrderFood.FoodId,
-                        Quantity = package.OrderFood.Quantity
-                    } : null,
-                    OrderDrink = package.OrderDrink != null ? new OrderDrink
-                    {
-                        Id = package.OrderDrink.Id,
-                        DrinkId = package.OrderDrink.DrinkId,
-                        Quantity = package.OrderDrink.Quantity
-                    } : null,
-                    OrderService = package.OrderService != null ? new OrderService
-                    {
-                        Id = package.OrderService.Id,
-                        ServiceId = package.OrderService.ServiceId,
-                        Quantity = package.OrderService.Quantity
-                    } : null
-                    // If there are more fields, map them accordingly
+                    Status = package.Status
                 }).SingleOrDefault(x => x.Id == id);
                 }
             }
@@ -158,32 +82,9 @@ namespace DataAccess
                 {
                     Id = package.Id,
                     PackageName = package.PackageName,
-                    OrderDrinkId = package.OrderDrinkId,
-                    OrderFoodId = package.OrderFoodId,
-                    OrderServiceId = package.OrderServiceId,
                     UserId = package.UserId,
                     PackageCategoryId = package.PackageCategoryId,
-                    Status = package.Status,
-                    // Assuming you have navigation properties in your Package entity
-                    OrderFood = package.OrderFood != null ? new OrderFood
-                    {
-                        Id = package.OrderFood.Id,
-                        FoodId = package.OrderFood.FoodId,
-                        Quantity = package.OrderFood.Quantity
-                    } : null,
-                    OrderDrink = package.OrderDrink != null ? new OrderDrink
-                    {
-                        Id = package.OrderDrink.Id,
-                        DrinkId = package.OrderDrink.DrinkId,
-                        Quantity = package.OrderDrink.Quantity
-                    } : null,
-                    OrderService = package.OrderService != null ? new OrderService
-                    {
-                        Id = package.OrderService.Id,
-                        ServiceId = package.OrderService.ServiceId,
-                        Quantity = package.OrderService.Quantity
-                    } : null
-                    // If there are more fields, map them accordingly
+                    Status = package.Status
                 }).ToList();
                 }
             }
