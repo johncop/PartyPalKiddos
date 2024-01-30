4d582d7d7f3ae38092c61f4d9754fba653b2ff76
johncop <thoanglong94@gmail.com>
2024-01-30 18:09:40 +0700

    add new Food, drink repository

---
+++ Repository/Repository/DrinkRepository.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObject.Models;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class DrinkRepository : IDrinkRepository
+    {
+        public void addDrink(Drink d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drink> GetAllDrinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drink GetDrinkById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drink> GetDrinkByName(string DrinkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeDrink(Drink d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDrink(Drink d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Repository/FoodRepository.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObject.Models;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class FoodRepository : IFoodReposiroty
+    {
+        public void addFood(Food f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Food> GetAllFoods()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Food GetFoodById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Food> GetFoodByName(string FoodName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeFood(Food f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFood(Food f)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
