2da3f34f8613044de8190204ba4716a6560a7388
johncop <thoanglong94@gmail.com>
2024-02-05 15:25:55 +0700

    Add new CRUD image

---
+++ Repository/Repository/ImageRepository/DrinkImageRepository.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObject.Models;
+using DataAccess.ImageDAO;
+using Repository.Interface.Image;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository.ImageRepository
+{
+    internal class DrinkImageRepository : IDrinkImageRepository
+    {
+        public void addDrinkImage(DrinkImage di) => DrinkImageDAO.SaveDrinkImage(di);
+
+        public List<DrinkImage> GetAllDrinkImages() => DrinkImageDAO.GetDrinkImages();
+
+        public DrinkImage GetDrinkImageById(int id) => DrinkImageDAO.findDrinkImageById(id);
+
+        public void removeDrinkImage(DrinkImage di) => DrinkImageDAO.DeleteDrinkImage(di);
+
+        public void UpdateDrinkImage(DrinkImage di) => DrinkImageDAO.UpdateDrinkImage(di);
+    }
+}
---
+++ Repository/Repository/ImageRepository/ServiceImageRepository.cs
@@ -0,0 +1,38 @@
+﻿using BusinessObject.Models;
+using Repository.Interface.Image;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository.ImageRepository
+{
+    public class ServiceImageRepository : IServiceImageRepository
+    {
+        public void addServiceImage(ServiceImage si)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ServiceImage> GetAllServiceImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceImage GetServiceImageById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeServiceImage(ServiceImage si)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateServiceImage(ServiceImage si)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
