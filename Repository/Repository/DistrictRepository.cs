58002c678fd85bc17f30f851cdfaede5cb4ebcd5
johncop <thoanglong94@gmail.com>
2024-02-05 15:25:36 +0700

    Add new CRUD district

---
+++ Repository/Repository/DistrictRepository.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObject.Models;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class DistrictRepository : IDistrictRepository
+    {
+        public void addDistrict(District d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<District> GetAllDistricts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public District GetDistrictById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<District> GetDistrictByName(string DistrictName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeDistrict(District d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDistrict(District d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
