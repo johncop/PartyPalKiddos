95213a727cf3469ea45fdf1d3dced83a39534afd
johncop <thoanglong94@gmail.com>
2024-03-12 16:24:37 +0700

    add new crud for combo food detail

---
+++ DataAccess/ComboFoodDetailDAO.cs
@@ -0,0 +1,123 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ComboFoodDetailDAO
+    {
+        #region query
+        public static List<ComboFoodDetail> GetComboFoodDetails()
+        {
+            var listComboFoodDetails = new List<ComboFoodDetail>();
+            try
+            {
+                using (var context = new PartyPalKiddosDBContext())
+                {
+                    listComboFoodDetails = context.ComboFoodDetails
+                .Select(ComboFoodDetail => new ComboFoodDetail
+                {
+                    ComboId= ComboFoodDetail.ComboId,
+                    Combo = ComboFoodDetail.Combo,
+                    FoodId= ComboFoodDetail.FoodId,
+                    Food = ComboFoodDetail.Food,
+                    Quantity = ComboFoodDetail.Quantity,
+                }).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return listComboFoodDetails;
+        }
+
+        public static List<ComboFoodDetail> findComboFoodDetailByComboId(int comboId)
+        {
+            List<ComboFoodDetail> f = new List<ComboFoodDetail>();
+            try
+            {
+                using (var context = new PartyPalKiddosDBContext())
+                {
+                    f = context.ComboFoodDetails
+                     .Where(ComboFoodDetail => ComboFoodDetail.ComboId == comboId)
+                .Select(ComboFoodDetail => new ComboFoodDetail
+                {
+                    ComboId = ComboFoodDetail.ComboId,
+                    Combo = ComboFoodDetail.Combo,
+                    FoodId = ComboFoodDetail.FoodId,
+                    Food = ComboFoodDetail.Food,
+                    Quantity = ComboFoodDetail.Quantity,
+                }).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return f;
+        }
+        #endregion
+
+
+
+        #region command
+        public static void SaveComboFoodDetail(ComboFoodDetail f)
+        {
+            try
+            {
+                using (var context = new PartyPalKiddosDBContext())
+                {
+                    context.ComboFoodDetails.Add(f);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static void DeleteComboFoodDetail(ComboFoodDetail comboFoodDetail)
+        {
+            try
+            {
+                using (var context = new PartyPalKiddosDBContext())
+                {
+                    var p1 = context.ComboFoodDetails.SingleOrDefault(x => x.ComboId == comboFoodDetail.ComboId);
+                    context.ComboFoodDetails.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static void UpdateComboFoodDetail(ComboFoodDetail comboFoodDetail)
+        {
+            try
+            {
+                using (var context = new PartyPalKiddosDBContext())
+                {
+                    context.Entry<ComboFoodDetail>(comboFoodDetail).State =
+                        Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Repository/Repository/ComboFoodDetailRepository.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class ComboFoodDetailRepository : IComboFoodDetailRepository
+    {
+        public void addComboFoodDetail(ComboFoodDetail comboFoodDetail) => ComboFoodDetailDAO.SaveComboFoodDetail(comboFoodDetail);
+
+        public List<ComboFoodDetail> GetAllComboFoodDetail() => ComboFoodDetailDAO.GetComboFoodDetails();
+
+        public List<ComboFoodDetail> GetComboFoodDetailByComboId(int comboId) => ComboFoodDetailDAO.findComboFoodDetailByComboId(comboId);
+
+        public void removeComboFoodDetail(ComboFoodDetail comboFoodDetail) => ComboFoodDetailDAO.DeleteComboFoodDetail(comboFoodDetail);
+
+        public void UpdateComboFoodDetail(ComboFoodDetail comboFoodDetail) => ComboFoodDetailDAO.UpdateComboFoodDetail(comboFoodDetail);
+    }
+}
