360560db5203c82c5e38ccc6fd33fe5d1416d571
johncop <thoanglong94@gmail.com>
2024-03-16 08:27:36 +0700

    add dtos

--- BusinessObject/DTO/addHostComboDetail.cs
+++ BusinessObject/DTO/addHostComboDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BusinessObject.DTO
 {
     public class addHostComboDetail
     {
+        [Key]
         public int? HostId { get; set; }
         public int? ComboId { get; set; }
         public int? FoodId { get; set; }
