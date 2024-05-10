3ca484cc80d6e6cb5bcb260fc482bb97c8803e2e
ICRRevengers <sogoku1113@gmail.com>
2024-05-10 22:04:47 +0700

    fix bug datatables

--- Front-end/src/components/common/table/tableAdmin.jsx
+++ Front-end/src/components/common/table/tableAdmin.jsx
@@ -1,9 +1,9 @@
-import { DataTable } from "simple-datatables";
 import { useEffect, useState } from "react";
 import { AddButton } from "../button/addItem";
 import { InputCommon } from "../input";
 import { useDispatch } from "react-redux";
 import { SET_STATUS_POPUP } from "../../../constants";
+import DataTable from "react-data-table-component";
 
 export const TableAdmin = ({
   state,
@@ -32,20 +32,20 @@ export const TableAdmin = ({
     event: null,
     item: null,
   });
-  useEffect(() => {
-    if (data.length > 0) {
-      new DataTable("#datatablesSimple", { perPage: 15 });
-      const elementsArray = document.querySelectorAll(".button-edit-item");
-      elementsArray.forEach(function (elem) {
-        elem.addEventListener("click", function (e) {
-          const dataIndex = data.findIndex(
-            (item) => item.id === Number(elem.getAttribute("data-id"))
-          );
-          modalEdit(data[dataIndex], dataIndex);
-        });
-      });
-    }
-  }, [data]);
+  // useEffect(() => {
+  //   if (data.length > 0) {
+  //     new DataTable("#datatablesSimple", { perPage: 15 });
+  //     const elementsArray = document.querySelectorAll(".button-edit-item");
+  //     elementsArray.forEach(function (elem) {
+  //       elem.addEventListener("click", function (e) {
+  //         const dataIndex = data.findIndex(
+  //           (item) => item.id === Number(elem.getAttribute("data-id"))
+  //         );
+  //         modalEdit(data[dataIndex], dataIndex);
+  //       });
+  //     });
+  //   }
+  // }, [data]);
   function getValue(type, key, data, multiple) {
     if (type === "file") {
       return null;
@@ -226,6 +226,39 @@ export const TableAdmin = ({
     });
   };
 
+  const tableColumns = columns.map((col, index) => {
+    return {
+      name: col,
+      selector: (dt) => getValue(null, columnKeys[index], dt, false),
+      sortable: true,
+    };
+  });
+
+  tableColumns.push({
+    name: "Status",
+    selector: (dt, index) => {
+      return (
+        <>
+          <button
+            className="button-edit-item"
+            id={`button-edit-item-${dt.id}`}
+            data-id={dt.id}
+            onClick={() => modalEdit(dt, index)}
+            style={{ backgroundColor: "transparent" }}
+          >
+            <i className="fa fa-edit me-1"></i>
+          </button>
+          <i
+            className="fa fa-trash"
+            onClick={() => {}}
+            data-bs-toggle="modal"
+            data-bs-target={"#modalRemoveItem" + dt.id}
+          ></i>
+        </>
+      );
+    },
+  });
+
   return (
     <div className="card mb-4">
       <div className="card-header">
@@ -233,45 +266,15 @@ export const TableAdmin = ({
         <AddButton fields={btnDataAdd} handleSubmit={handleSubmit} />
       </div>
       <div className="card-body">
-        <table id="datatablesSimple">
-          <thead>
-            <tr>
-              {columns.map((column, index) => (
-                <th key={"colum-table-1-" + index}>{column}</th>
-              ))}
-              <th className="text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => {
-              return (
-                <tr key={"item-value-" + index}>
-                  {columnKeys.map((column, i) => (
-                    <td key={"colum-table-" + index + "-" + i}>
-                      {getValue(null, column, item, false)}
-                    </td>
-                  ))}
-                  <td className="text-center">
-                    <button
-                      className="button-edit-item"
-                      id={`button-edit-item-${item.id}`}
-                      data-id={item.id}
-                      style={{ backgroundColor: "transparent" }}
-                    >
-                      <i className="fa fa-edit me-1"></i>
-                    </button>
-                    <i
-                      className="fa fa-trash"
-                      onClick={() => {}}
-                      data-bs-toggle="modal"
-                      data-bs-target={"#modalRemoveItem" + item.id}
-                    ></i>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <DataTable
+          columns={tableColumns}
+          data={data}
+          pagination={true}
+          onRowsPerPage={10}
+          dense
+          fixedHeader={true}
+          width={"100vh"}
+        />
       </div>
 
       {data.map((item, index) => (
